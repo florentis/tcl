2faf1e2083ca6421f9f1ce2c89a4dcfcb16210c9
florentis <florentmerlet73@gmail.com>
2026-04-04 10:55:56 +0200

    Add files via upload

--- install_SH/bin/tests/determinant.tcl
+++ install_SH/bin/tests/determinant.tcl
@@ -29,7 +29,7 @@ proc determinant_Orig matrix {
     }
  }
  proc cancelcol {n matrix} {
-    set res {}
+     set res [list]
     foreach row $matrix {
         lappend res [lreplace $row $n $n]
     }
@@ -62,19 +62,48 @@ proc determinant_SH1 matrix {
     }
 }
 
+proc determinant_SH2 matrix {
+    if {[llength $matrix] != [llength [lindex $matrix 0]]} {
+	error "non-square matrix"
+    }
+    switch [llength $matrix] {
+	2 {(
+	    [set Map {{a b} {c d}};
+		  lmap map $Map row $matrix {
+		      lmap var $map val $row {($var=$val)}
+		  }];
+	    $a*$d - $b*$c
+	)} default {(
+		i = res = 0;
+		mat2=[lrange $matrix 1 end];
+		[foreach element [lindex $matrix 0] {
+		    if $element {(
+			sign = $i%2? -1: 1;
+			res =$res + $sign*$element* [determinant_SH1 [cancelcol $i $mat2]]
+			)}
+		    incr i
+		}];
+	    $res
+	 )}	    
+    }
+}
+
 set chan [open ./tests/determinant.log w]
-for (i="speed(Orig)"="speed(SH1)"=0) {$i<3} {incr i} {(
+for (i="speed(Orig)"="speed(SH1)"="speed(SH2)"=0) {$i<3} {incr i} {(
     "speed(Orig)" = $speed(Orig) + [lindex [timerate [list determinant_Orig {{5 -3 2} {1 0 6} {3 1 -2}}]] 0] ;
-    "speed(SH1)" = $speed(SH1) + [lindex [timerate [list determinant_SH1 {{5 -3 2} {1 0 6} {3 1 -2}}]] 0]
+    "speed(SH1)" = $speed(SH1) + [lindex [timerate [list determinant_SH1 {{5 -3 2} {1 0 6} {3 1 -2}}]] 0];
+    "speed(SH2)" = $speed(SH2) + [lindex [timerate [list determinant_SH2 {{5 -3 2} {1 0 6} {3 1 -2}}]] 0]
     )}
 
-foreach suffix {Orig SH1} {("speed($suffix)" = [set speed($suffix)]/3)}
+foreach suffix {Orig SH1 SH2} {("speed($suffix)" = [set speed($suffix)]/3)}
 
 puts $chan Results
 puts $chan "Original Proc : speed = $speed(Orig)"
 puts $chan "Original Proc : result : [determinant_Orig {{5 -3 2} {1 0 6} {3 1 -2}}]"
 puts $chan "SH1 Proc : speed = $speed(SH1)"
 puts $chan "SH1 Proc : result = [determinant_SH1 {{5 -3 2} {1 0 6} {3 1 -2}}]"
+puts $chan "SH2 Proc : speed = $speed(SH2)"
+puts $chan "SH2 Proc : result = [determinant_SH2 {{5 -3 2} {1 0 6} {3 1 -2}}]"
 puts $chan -------------------------------------------------------------------------
 puts $chan Bytecodes
 puts $chan -------------------------------------------------------------------------
@@ -83,5 +112,8 @@ puts $chan [tcl::unsupported::disassemble proc determinant_Orig]
 puts $chan -------------------------------------------------------------------------
 puts $chan 1.SH1,\ length=[string length [info body determinant_SH1]]
 puts $chan [tcl::unsupported::disassemble proc determinant_SH1]
+puts $chan -------------------------------------------------------------------------
+puts $chan 1.SH2,\ length=[string length [info body determinant_SH2]]
+puts $chan [tcl::unsupported::disassemble proc determinant_SH2]
 
 close $chan
